3eae7d72809730930a6681df4e9dadfc62cb330b
wayfarerx <x@wayfarerx.net>
2023-03-31 22:30:56 -0600

    Project setup.

---
+++ build.sbt
@@ -0,0 +1,45 @@
+import Dependencies.*
+
+ThisBuild / organization := "net.wayfarerx"
+ThisBuild / version := "0.2.0-SNAPSHOT"
+ThisBuild / scalaVersion := "3.2.2"
+
+/** The aggregation of the projects that define cf4s. */
+lazy val cf4s = (project in file(".")).aggregate(
+  generator,
+  common,
+  s3
+)
+
+/** The generator for cf4s. */
+lazy val generator = project.settings(
+  name := "cf4s-generator",
+  libraryDependencies ++= Seq(
+    AwsCore,
+    CirceCore,
+    CirceParser,
+    Refined,
+    Scopt,
+    Zio,
+    ZioLogging,
+    ZioTest,
+    ZioTestSbt,
+    ZioTestMagnolia
+  ),
+  testFrameworks += new TestFramework("zio.test.sbt.ZTestFramework"),
+  Compile / run / fork := true,
+  mainClass := Some("net.wayfarerx.cf4s.generator.main.Main")
+)
+
+/** The common model for cf4s. */
+lazy val common = project.settings(
+  name := "cf4s-common",
+  libraryDependencies ++= Seq(
+    CirceCore
+  )
+)
+
+/** The S3 model for cf4s. */
+lazy val s3 = project.settings(
+  name := "cf4s-s3"
+).dependsOn(common)
---
+++ project/Dependencies.scala
@@ -0,0 +1,29 @@
+import sbt.*
+object Dependencies {
+
+  val AwsGroup = "com.amazonaws"
+  val AwsVersion = "1.12.429"
+  lazy val AwsCore = AwsGroup % "aws-java-sdk-core" % AwsVersion
+
+  val CirceGroup = "io.circe"
+  val CirceVersion = "0.14.5"
+  lazy val CirceCore = CirceGroup %% "circe-core" % CirceVersion
+  lazy val CirceParser = CirceGroup %% "circe-parser" % CirceVersion
+
+  val RefinedGroup = "eu.timepit"
+  val RefinedVersion = "0.10.3"
+  lazy val Refined = RefinedGroup %% "refined" % RefinedVersion
+  lazy val RefinedCats = RefinedGroup %% "refined-cats" % RefinedVersion
+
+  lazy val Scopt = "com.github.scopt" %% "scopt" % "4.1.0"
+
+  val ZioGroup = "dev.zio"
+  val ZioVersion = "2.0.10"
+  lazy val Zio = ZioGroup %% "zio" % ZioVersion
+  lazy val ZioInteropCats = ZioGroup %% "zio-interop-cats" % "23.0.0.1"
+  lazy val ZioLogging = ZioGroup %% "zio-logging" % "2.1.11"
+  lazy val ZioTest = ZioGroup %% "zio-test" % ZioVersion % Test
+  lazy val ZioTestSbt = ZioGroup %% "zio-test-sbt" % ZioVersion % Test
+  lazy val ZioTestMagnolia = ZioGroup %% "zio-test-magnolia" % ZioVersion % Test
+
+}
